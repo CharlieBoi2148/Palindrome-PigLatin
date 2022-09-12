7103e2cfe75160faf61079f51a62fc18dc246722
Charlie B <89105051+CharlieBoi2148@users.noreply.github.com>
2022-09-12 01:22:19 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String S3 = "there";
+      
+//        String S4="that";
+//        System.out.println(S3.charAt(0)=='t');
+
+        System.out.println(StringUtil.reverseStringUtil("Hello There Dude"));
+        System.out.println(StringUtil.palindromeChecker(("radar")));
+        System.out.println(StringUtil.pigLatin("Hello There my name is Jerry boi"));
+        System.out.println(StringUtil.shortHand("This phrase is for you and to you my boi"));
+        }
+    }
